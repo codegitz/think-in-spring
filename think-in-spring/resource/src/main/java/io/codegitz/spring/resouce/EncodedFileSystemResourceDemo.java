ec6b96e34d32edeaccc62abd0c351826bf003c02
Codegitz <codegit@foxmail.com>
2020-09-29 18:05:22 +0800

    Spring Resource接口扩展 & Spring 资源加载器

---
+++ think-in-spring/resource/src/main/java/io/codegitz/spring/resouce/EncodedFileSystemResourceDemo.java
@@ -0,0 +1,32 @@
+package io.codegitz.spring.resouce;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.support.EncodedResource;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * 带有字符编码的 {@link org.springframework.core.io.FileSystemResource} 示例
+ * @author 张观权
+ * @date 2020/9/29 16:46
+ *
+ * @see org.springframework.core.io.FileSystemResource
+ * @see org.springframework.core.io.support.EncodedResource
+ **/
+public class EncodedFileSystemResourceDemo {
+    public static void main(String[] args) throws IOException {
+        String contentPath = "C:\\my_study_project\\spring-geek\\think-in-spring\\resource\\src\\main\\java\\io\\codegitz\\spring\\resouce\\EncodedFileSystemResourceDemo.java";
+        File file = new File(contentPath);
+        // FileSystemResource => WritableResource => Resource
+        FileSystemResource fileSystemResource = new FileSystemResource(contentPath);
+        EncodedResource encodedResource = new EncodedResource(fileSystemResource,"UTF-8");
+        // 字符输入流
+        try(Reader reader = encodedResource.getReader()){
+            System.out.println(IOUtils.toString(reader));
+        }
+
+    }
+}
---
+++ think-in-spring/resource/src/main/java/io/codegitz/spring/resouce/EncodedFileSystemResourceLoaderDemo.java
@@ -0,0 +1,35 @@
+package io.codegitz.spring.resouce;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.FileSystemResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.EncodedResource;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * 带有字符编码的 {@link FileSystemResourceLoader} 示例
+ * @author 张观权
+ * @date 2020/9/29 16:46
+ *
+ * @see FileSystemResourceLoader
+ * @see FileSystemResource
+ * @see EncodedResource
+ **/
+public class EncodedFileSystemResourceLoaderDemo {
+    public static void main(String[] args) throws IOException {
+        String contentPath = "C:\\my_study_project\\spring-geek\\think-in-spring\\resource\\src\\main\\java\\io\\codegitz\\spring\\resouce\\EncodedFileSystemResourceDemo.java";
+        // FileSystemResource => WritableResource => Resource
+        FileSystemResourceLoader fileSystemResourceLoader = new FileSystemResourceLoader();
+        Resource resource = fileSystemResourceLoader.getResource(contentPath);
+        EncodedResource encodedResource = new EncodedResource(resource,"UTF-8");
+        // 字符输入流
+        try(Reader reader = encodedResource.getReader()){
+            System.out.println(IOUtils.toString(reader));
+        }
+
+    }
+}
