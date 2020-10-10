bd891872dab82f7e464bdd98e25affa340c6d2ec
Codegitz <codegit@foxmail.com>
2020-10-10 13:33:30 +0800

    Spring Validator 的救赎

--- think-in-spring/validation/src/main/java/io/codegitz/spring/validation/SpringBeanValidationDemo.java
+++ think-in-spring/validation/src/main/java/io/codegitz/spring/validation/SpringBeanValidationDemo.java
@@ -1,12 +1,19 @@
 package io.codegitz.spring.validation;
 
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Validator;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 /**
  * Spring bean Validation 整合示例
+ *
  * @author 张观权
  * @date 2020/10/9 18:58
  * @see org.springframework.validation.Validator
@@ -18,10 +25,42 @@ public class SpringBeanValidationDemo {
 
         ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:META-INF\\bean-validation-context.xml");
 
-        Validator validator = applicationContext.getBean(Validator.class);
+//        Validator validator = applicationContext.getBean(Validator.class);
+//        System.out.println("validator instanceof LocalValidatorFactoryBean: " + (validator instanceof LocalValidatorFactoryBean));
 
-        System.out.println(validator instanceof LocalValidatorFactoryBean);
-
+        UserProcessor userProcessor = applicationContext.getBean(UserProcessor.class);
+        userProcessor.process(new User());
         applicationContext.close();
+    }
+
+//    @Configuration(proxyBeanMethods = false)
+    @Component
+    @Validated
+    static class UserProcessor{
+
+        public void process(@Valid User user){
+            System.out.println(user);
+        }
+    }
+
+    static class User{
+
+        @NotNull
+        private String userName;
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "userName='" + userName + '\'' +
+                    '}';
+        }
     }
 }
