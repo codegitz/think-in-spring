34ca1d972414e6726cf05c498b261c66ea9a7d14
Codegitz <codegit@foxmail.com>
2020-10-15 20:43:54 +0800

    SpringPropertyEditor的设计缺陷

--- think-in-spring/conversion/src/main/java/io/codegitz/spring/conversion/SpringCustomizedPropertyEditorDemo.java
+++ think-in-spring/conversion/src/main/java/io/codegitz/spring/conversion/SpringCustomizedPropertyEditorDemo.java
@@ -12,6 +12,12 @@ import java.beans.PropertyEditor;
  * @author 张观权
  * @date 2020/10/14 13:27
  * @see PropertyEditor
+ *
+ * Spring PropertyEditor 的设计缺陷
+ * 1.违反单一职责原则 -> java.beans.PropertyEditor 接口职责太多，除了类型转换，还包括Java Beans 事件和 Java GUI 交互
+ * 2.java.beans.PropertyEditor 实现类型的局限性 -> 来源只能是 java.lang.String 类型
+ * 3.java.beans.PropertyEditor 缺少类型安全 -> 除了实现类命名可以感知语义，实现类无法感知目标转换类型
+ *
  **/
 public class SpringCustomizedPropertyEditorDemo {
 
