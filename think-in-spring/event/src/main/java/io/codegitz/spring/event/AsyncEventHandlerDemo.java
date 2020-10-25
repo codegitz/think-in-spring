d29536b025eaf778594f598d49d812272cb4e58d
Codegitz <codegit@foxmail.com>
2020-10-25 21:15:08 +0800

    同步和异步spring 事件广播

--- think-in-spring/event/src/main/java/io/codegitz/spring/event/AsyncEventHandlerDemo.java
+++ think-in-spring/event/src/main/java/io/codegitz/spring/event/AsyncEventHandlerDemo.java
@@ -1,6 +1,5 @@
 package io.codegitz.spring.event;
 
-import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.ContextClosedEvent;
@@ -8,42 +7,69 @@ import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
-import sun.net.www.content.text.Generic;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+
+import static java.util.concurrent.Executors.newSingleThreadExecutor;
 
 /**
  * 异步事件处理器示例
- * @author 张观权
- * @date 2020/10/25 16:26
- **/
+ *
+ * @author
+ * @since
+ */
 public class AsyncEventHandlerDemo {
+
     public static void main(String[] args) {
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
 
-        applicationContext.addApplicationListener(new MySpringEventListener());
-        applicationContext.refresh(); // 初始化ApplicationEventMulticaster
+        GenericApplicationContext context = new GenericApplicationContext();
 
-        ApplicationEventMulticaster multicaster = applicationContext.getBean(AbstractApplicationContext.APPLICATION_EVENT_MULTICASTER_BEAN_NAME, ApplicationEventMulticaster.class);
-        if (multicaster instanceof SimpleApplicationEventMulticaster){
-            SimpleApplicationEventMulticaster simpleApplicationEventMulticaster = (SimpleApplicationEventMulticaster) multicaster;
-            ExecutorService executorService = Executors.newSingleThreadExecutor(new CustomizableThreadFactory("my-spring-event-execute-pool"));
+        // 1.添加自定义 Spring 事件监听器
+        context.addApplicationListener(new MySpringEventListener());
+
+        // 2.启动 Spring 应用上下文
+        context.refresh(); // 初始化 ApplicationEventMulticaster
+
+        // 依赖查找 ApplicationEventMulticaster
+        ApplicationEventMulticaster applicationEventMulticaster =
+                context.getBean(AbstractApplicationContext.APPLICATION_EVENT_MULTICASTER_BEAN_NAME, ApplicationEventMulticaster.class);
+
+        // 判断当前 ApplicationEventMulticaster 是否为 SimpleApplicationEventMulticaster
+        if (applicationEventMulticaster instanceof SimpleApplicationEventMulticaster) {
+            SimpleApplicationEventMulticaster simpleApplicationEventMulticaster =
+                    (SimpleApplicationEventMulticaster) applicationEventMulticaster;
+            // 切换 taskExecutor
+            ExecutorService taskExecutor = newSingleThreadExecutor(new CustomizableThreadFactory("my-spring-event-thread-pool"));
             // 同步 -> 异步
-            simpleApplicationEventMulticaster.setTaskExecutor(executorService);
-            // 添加 ContextClosedEvent 事件监听
-            applicationContext.addApplicationListener(new ApplicationListener<ContextClosedEvent>() {
+            simpleApplicationEventMulticaster.setTaskExecutor(taskExecutor);
+
+            // 添加 ContextClosedEvent 事件处理
+            applicationEventMulticaster.addApplicationListener(new ApplicationListener<ContextClosedEvent>() {
                 @Override
                 public void onApplicationEvent(ContextClosedEvent event) {
-                    if (!executorService.isShutdown()){
-                        executorService.shutdown();
+                    if (!taskExecutor.isShutdown()) {
+                        taskExecutor.shutdown();
                     }
-
                 }
             });
-        }
-        applicationContext.publishEvent(new MySpringEvent("Hello,world"));
 
-        applicationContext.close();
+            simpleApplicationEventMulticaster.setErrorHandler(e -> {
+                System.err.println("当 Spring 事件异常时，原因：" + e.getMessage());
+            });
+        }
+
+        context.addApplicationListener(new ApplicationListener<MySpringEvent>() {
+            @Override
+            public void onApplicationEvent(MySpringEvent event) {
+                throw new RuntimeException("故意抛出异常");
+            }
+        });
+
+        // 3. 发布自定义 Spring 事件
+        context.publishEvent(new MySpringEvent("Hello,World"));
+
+        // 4. 关闭 Spring 应用上下文（ContextClosedEvent）
+        context.close();
+
     }
 }
