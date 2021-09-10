8cd7242069819e875ef06e04c871523386713318
Codegitz <codegit@foxmail.com>
2021-09-10 18:55:06 +0800

    leetcode practice RotateList in Sep.10 2021

---
+++ think-in-spring/50CommonMistakes/src/main/java/io/codegitz/spring/common/mistakes/leetcode/RotateList.java
@@ -0,0 +1,116 @@
+package io.codegitz.spring.common.mistakes.leetcode;
+
+/**
+ * @author 张观权
+ * @date 2021/9/10 14:33
+ *
+ * 61. Rotate List
+ * Medium
+ *
+ * 3067
+ *
+ * 1191
+ *
+ * Add to List
+ *
+ * Share
+ * Given the head of a linked list, rotate the list to the right by k places.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [1,2,3,4,5], k = 2
+ * Output: [4,5,1,2,3]
+ * Example 2:
+ *
+ *
+ * Input: head = [0,1,2], k = 4
+ * Output: [2,0,1]
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the list is in the range [0, 500].
+ * -100 <= Node.val <= 100
+ * 0 <= k <= 2 * 109
+ *
+ **/
+public class RotateList {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        node1.next = node2;
+        ListNode node3 = new ListNode(3);
+        node2.next = node3;
+        ListNode node4 = new ListNode(4);
+        node3.next = node4;
+        ListNode node5 = new ListNode(5);
+        node4.next = node5;
+        ListNode listNode = rotateRight(node1, 5);
+        System.out.println(listNode);
+    }
+    public static ListNode rotateRight(ListNode head, int k) {
+        if (head == null || k == 0){
+            return head;
+        }
+        int length = 0;
+        ListNode tmp = head;
+        while (tmp != null){
+            length++;
+            tmp = tmp.next;
+        }
+        k = k % length;
+        if (k == 0){
+            return head;
+        }
+        ListNode pre = head;
+        ListNode cur = head;
+        // find the rotate point
+        while (k > 0){
+            if (pre == null){
+                pre = head;
+            }
+            pre = pre.next;
+            k--;
+        }
+        if (k == 0 && pre == null){
+            return head;
+        }
+        while (pre.next != null){
+            pre = pre.next;
+            cur = cur.next;
+        }
+        ListNode point = cur.next;
+        cur.next = null;
+        pre.next = head;
+        head = point;
+        return head;
+    }
+
+    public static ListNode rotate(ListNode head){
+        if (head == null){
+            return head;
+        }
+        ListNode cur = head;
+        ListNode next = head.next;
+        ListNode tmp;
+        cur.next = null;
+        while (next != null){
+            tmp = next.next;
+            next.next = cur;
+            cur = next;
+            next = tmp;
+        }
+        return cur;
+    }
+
+   static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+}
