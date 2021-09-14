0b3a78ff2c922e034acb7afb12ddcce3e65b7674
Codegitz <codegit@foxmail.com>
2021-09-14 11:27:45 +0800

    leetcode practice(copy) ValidNumber Sep.14 2021

---
+++ think-in-spring/50CommonMistakes/src/main/java/io/codegitz/spring/common/mistakes/leetcode/AddBinary.java
@@ -0,0 +1,39 @@
+package io.codegitz.spring.common.mistakes.leetcode;
+
+/**
+ * @author 张观权
+ * @date 2021/9/14 11:27
+ *
+ * 67. Add Binary
+ * Easy
+ *
+ * 3370
+ *
+ * 399
+ *
+ * Add to List
+ *
+ * Share
+ * Given two binary strings a and b, return their sum as a binary string.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: a = "11", b = "1"
+ * Output: "100"
+ * Example 2:
+ *
+ * Input: a = "1010", b = "1011"
+ * Output: "10101"
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= a.length, b.length <= 104
+ * a and b consist only of '0' or '1' characters.
+ * Each string does not contain leading zeros except for the zero itself.
+ *
+ **/
+public class AddBinary {
+}
