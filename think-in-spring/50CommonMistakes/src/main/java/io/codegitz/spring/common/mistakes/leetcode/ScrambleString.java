dd48395943462361a0f792d1674ef485aab0b626
Codegitz <codegit@foxmail.com>
2021-09-26 16:54:30 +0800

    leetcode practice ScrambleString Sep.26 2021

---
+++ think-in-spring/50CommonMistakes/src/main/java/io/codegitz/spring/common/mistakes/leetcode/ScrambleString.java
@@ -0,0 +1,107 @@
+package io.codegitz.spring.common.mistakes.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 张观权
+ * @date 2021/9/23 13:14
+ *
+ * 87. Scramble String
+ * Hard
+ *
+ * 1101
+ *
+ * 877
+ *
+ * Add to List
+ *
+ * Share
+ * We can scramble a string s to get a string t using the following algorithm:
+ *
+ * If the length of the string is 1, stop.
+ * If the length of the string is > 1, do the following:
+ * Split the string into two non-empty substrings at a random index, i.e., if the string is s, divide it to x and y where s = x + y.
+ * Randomly decide to swap the two substrings or to keep them in the same order. i.e., after this step, s may become s = x + y or s = y + x.
+ * Apply step 1 recursively on each of the two substrings x and y.
+ * Given two strings s1 and s2 of the same length, return true if s2 is a scrambled string of s1, otherwise, return false.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s1 = "great", s2 = "rgeat"
+ * Output: true
+ * Explanation: One possible scenario applied on s1 is:
+ * "great" --> "gr/eat" // divide at random index.
+ * "gr/eat" --> "gr/eat" // random decision is not to swap the two substrings and keep them in order.
+ * "gr/eat" --> "g/r / e/at" // apply the same algorithm recursively on both substrings. divide at ranom index each of them.
+ * "g/r / e/at" --> "r/g / e/at" // random decision was to swap the first substring and to keep the second substring in the same order.
+ * "r/g / e/at" --> "r/g / e/ a/t" // again apply the algorithm recursively, divide "at" to "a/t".
+ * "r/g / e/ a/t" --> "r/g / e/ a/t" // random decision is to keep both substrings in the same order.
+ * The algorithm stops now and the result string is "rgeat" which is s2.
+ * As there is one possible scenario that led s1 to be scrambled to s2, we return true.
+ * Example 2:
+ *
+ * Input: s1 = "abcde", s2 = "caebd"
+ * Output: false
+ * Example 3:
+ *
+ * Input: s1 = "a", s2 = "a"
+ * Output: true
+ *
+ *
+ * Constraints:
+ *
+ * s1.length == s2.length
+ * 1 <= s1.length <= 30
+ * s1 and s2 consist of lower-case English letters.
+ *
+ **/
+public class ScrambleString {
+    public static void main(String[] args) {
+        boolean scramble = isScramble("abc", "bca");
+        System.out.println(scramble);
+    }
+    public static boolean isScramble(String s1, String s2) {
+        if (s1 == null || s2 == null){
+            return false;
+        }
+        if (s1.equals(s2)){
+            return true;
+        }
+        if (!Counter(s1,s2)){
+            return false;
+        }
+        for (int i = 1; i < s1.length(); i++) {
+            if (isScramble(s1.substring(0,i),s2.substring(0,i)) && isScramble(s1.substring(i),s2.substring(i))){
+                return true;
+            }
+
+            if (isScramble(s1.substring(0,i),s2.substring(s1.length() - i)) && isScramble(s1.substring(i),s2.substring(0,s1.length() - i))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean Counter(String s1, String s2) {
+        if (s1.length() != s2.length()){
+            return false;
+        }
+        int[] cache = new int[128];
+        for (int i = 0; i < s1.length(); i++) {
+            cache[s1.charAt(i)]++;
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            cache[s2.charAt(i)]--;
+        }
+        for (int i = 0; i < cache.length; i++) {
+            if (cache[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
